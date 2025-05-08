bda915f2e682f88de838fe98434e7bf4b3a0370d
Dzmitry <111795652+Dzmitryy1812@users.noreply.github.com>
2025-05-08 14:40:09 +0300

    Create  blockchain.js

---
+++ blockchain.js
@@ -0,0 +1,258 @@
+// blockchain.js
+import { ethers } from 'https://cdn.jsdelivr.net/npm/ethers@5.7.2/dist/ethers.esm.min.js';
+
+const contractAddress = '0x715045cea81d1dcc604b6a379b94d6049cdaa5a0';
+const contractABI = [
+  {
+    "inputs": [
+      {
+        "internalType": "string",
+        "name": "_cid",
+        "type": "string"
+      }
+    ],
+    "name": "addMeme",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "stateMutability": "nonpayable",
+    "type": "constructor"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "memeId",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "string",
+        "name": "cid",
+        "type": "string"
+      }
+    ],
+    "name": "MemeAdded",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_memeId",
+        "type": "uint256"
+      }
+    ],
+    "name": "vote",
+    "outputs": [],
+    "stateMutability": "nonpayable",
+    "type": "function"
+  },
+  {
+    "anonymous": false,
+    "inputs": [
+      {
+        "indexed": false,
+        "internalType": "uint256",
+        "name": "memeId",
+        "type": "uint256"
+      },
+      {
+        "indexed": false,
+        "internalType": "address",
+        "name": "voter",
+        "type": "address"
+      }
+    ],
+    "name": "Voted",
+    "type": "event"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_memeId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getCid",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "",
+        "type": "string"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "_memeId",
+        "type": "uint256"
+      }
+    ],
+    "name": "getVotes",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      },
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "hasVoted",
+    "outputs": [
+      {
+        "internalType": "bool",
+        "name": "",
+        "type": "bool"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "memeCount",
+    "outputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [
+      {
+        "internalType": "uint256",
+        "name": "",
+        "type": "uint256"
+      }
+    ],
+    "name": "memes",
+    "outputs": [
+      {
+        "internalType": "string",
+        "name": "cid",
+        "type": "string"
+      },
+      {
+        "internalType": "uint256",
+        "name": "votes",
+        "type": "uint256"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  },
+  {
+    "inputs": [],
+    "name": "owner",
+    "outputs": [
+      {
+        "internalType": "address",
+        "name": "",
+        "type": "address"
+      }
+    ],
+    "stateMutability": "view",
+    "type": "function"
+  }
+];
+
+let provider;
+let signer;
+let contract;
+
+export async function connectWallet() {
+  if (!window.ethereum) {
+    alert('Пожалуйста, установите MetaMask!');
+    return false;
+  }
+
+  provider = new ethers.providers.Web3Provider(window.ethereum);
+  await provider.send('eth_requestAccounts', []);
+  signer = provider.getSigner();
+  contract = new ethers.Contract(contractAddress, contractABI, signer);
+
+  return true;
+}
+
+export async function loadMemes() {
+  const count = (await contract.memeCount()).toNumber();
+  const memes = [];
+
+  for (let i = 1; i <= count; i++) {
+    const meme = await contract.memes(i);
+    memes.push({
+      id: i,
+      cid: meme.cid,
+      votes: meme.votes.toNumber()
+    });
+  }
+
+  return memes;
+}
+
+export async function voteMeme(memeId) {
+  try {
+    const userAddress = await signer.getAddress();
+    const voted = await contract.hasVoted(userAddress, memeId);
+    if (voted) {
+      alert('Вы уже голосовали за этот мем.');
+      return false;
+    }
+
+    const tx = await contract.vote(memeId);
+    await tx.wait();
+    alert('Голос успешно отдан!');
+    return true;
+  } catch (error) {
+    alert('Ошибка при голосовании: ' + error.message);
+    return false;
+  }
+}
+
+export async function addMeme(cid) {
+  try {
+    const tx = await contract.addMeme(cid);
+    await tx.wait();
+    alert('Мем успешно добавлен!');
+    return true;
+  } catch (error) {
+    alert('Ошибка при добавлении мемa: ' + error.message);
+    return false;
+  }
+}
